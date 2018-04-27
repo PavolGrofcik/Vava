91d3b8e8f1eb0b6798d64d810805ce1ccbcbd48a
xheban <36968445+xheban@users.noreply.github.com>
2018-04-27 16:11:32 +0200

    Revert "Merge branch 'master' of https://github.com/PavolGrofcik/Vava"
    This reverts commit 36f0e149cddaf0b5033be6d78b96565edb63f0a0, reversing
    changes made to 4c06a6c7ca82346a86453c502564d151494cf73d.

--- src/main/view/MainView.java
+++ src/main/view/MainView.java
@@ -25,11 +25,7 @@ public class MainView extends Application {
 	private TextField user_text = new TextField();
 	private PasswordField password_text = new PasswordField();
 	private Button login = new Button("Login");
-<<<<<<< HEAD
 	Image background = new Image("File:resource/login2.png");
-=======
-	Image background = new Image("File:resource/logo.png");
->>>>>>> 4388c31832fc6589d9d91b8d781fe4c6d2fc3d8b
 	ImageView iv = new ImageView();
 	
 	
@@ -78,7 +74,6 @@ public class MainView extends Application {
 	
 		Scene scene = new Scene(pane,450,300);
 		primaryStage.setTitle("Login");
-		primaryStage.getIcons().add(new Image("File:resource/logo.png"));
 		primaryStage.setScene(scene);
 		primaryStage.setResizable(false);
 		primaryStage.show();
