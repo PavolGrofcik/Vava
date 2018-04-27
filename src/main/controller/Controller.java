537c4ad418b65e817a6836fba8bf440e37c73b34
PavolGrofcik <grofcik.pavol@gmail.com>
2018-04-27 18:05:23 +0200

    Login working

--- src/main/controller/Controller.java
+++ src/main/controller/Controller.java
@@ -1,8 +1,20 @@
 package main.controller;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import main.entities.Account;
 import main.entities.Customer;
 import main.entities.Event;
 
@@ -15,7 +27,7 @@ import main.entities.Event;
 
 public class Controller {
 	
-	private SessionFactory factory;
+	private static SessionFactory factory;
 	private static Controller controller = new Controller();
 	
 	
@@ -23,36 +35,72 @@ public class Controller {
 		this.factory = HibernateUtil.getSessionFactory();
 	}
 
-	
-	//Metóda vráti inštanciu pre Controller
 	public static Controller getInstance() {
 		return controller;
 	}
 	
-	
-	public static void main(String[] args) {
-		Controller controller = getInstance();
-		Customer cust;
-		Event event;
-		
-		 Session session = controller.factory.openSession();
-	     Transaction tx = session.beginTransaction();
-	     
-	     /*TypedQuery<Customer> query = session.createQuery("SELECT new Customer(id ,firstName, lastName, date, sex, telNumber, city, address)" +  ""
-	     		+ "FROM Customer WHERE id = :param1", Customer.class);
-	     query.setParameter("param1", 1);
-	     */
-	     event=session.get(Event.class, 1);
-	     //cust = query.getResultList().get(0);
-	     cust = session.get(Customer.class, 1);
-	     
-	     tx.commit();
-				
-		event.showEventInfo();
-		cust.showCustomerInfo();
-	     
-	     session.close();
-	     controller.factory.close();						//Ukonèenie session nad DB
+	public void shutDown() {
+		factory.close();
 	}
-
+	
+	public int loginCustomer(TextField username, PasswordField password) throws NoSuchAlgorithmException {
+		
+		Session session = factory.openSession();
+		Transaction transaction = session.beginTransaction();
+	
+		if(!checkInputs(username.getText(), password.getText())) {
+			return -1;	// Missing values
+		}
+		
+		String usernme = username.getText();
+		String passwd;
+		Account account;
+		
+		try {
+			
+			TypedQuery<Account> query = session.createQuery("SELECT new Account(customerId, controlQuestionId, " +
+					"cash, username,password,answer) FROM Account WHERE username = :arg");
+			query.setParameter("arg", usernme);
+			
+			if(query.getResultList().isEmpty()) {
+				return - 2;		// Username does not exist
+			}
+			
+			
+			account = query.getResultList().get(0);
+			
+			String testString= password.getText();
+	        MessageDigest md = MessageDigest.getInstance("MD5");
+	        byte[] messageDigest = md.digest(testString.getBytes());
+	        BigInteger number = new BigInteger(1, messageDigest);
+	        String hashtext = number.toString(16);
+	        
+			System.out.println(hashtext);
+			
+			if(!hashtext.equals(account.getPassword())) {
+				return -2;		// Invalid password
+			}
+			
+			transaction.commit();
+			
+		} catch (HibernateException e) {
+			e.printStackTrace();
+			transaction.rollback();
+			return -3;		// Error
+			
+		}finally {
+			session.close();
+		}
+		
+		return 1;
+	}
+	
+	
+	private boolean checkInputs(String username, String password) {
+		if(username.length() <=0 || password.length() <= 0) {
+			return false;
+		}
+		
+		return true;
+	}
 }
